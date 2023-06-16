3c6b4de1d65ba17ccd7d4d731956b4f630397ebf
purushottam-bajaj <66436394+purushottam-bajaj@users.noreply.github.com>
2023-06-16 16:24:11 +0530

    Update Tenure Calculation Function.py

--- Tenure Calculation Function.py
+++ Tenure Calculation Function.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[44]:
-
-
 import pymongo
 client = pymongo.MongoClient('mongodb://embifiAdmin:embifi_1659709763@db.embifi.in:22058/lms?authMechanism=DEFAULT&authSource=admin')
 db = client['lms']
